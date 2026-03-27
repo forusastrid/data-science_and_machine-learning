1cf2e2af8ada108b25899e75465ef61b36842048
김희성 <forusastrid@gmail.com>
2026-03-27 10:05:14 +0900

    Create day14.py

---
+++ summary/day14.py
@@ -0,0 +1,36 @@
+#85
+
+linear_svm_3d = LinearSVC(max_iter=5000).fit(X_new, y)
+coef, intercept = linear_svm_3d.coef_.ravel(), linear_svm_3d.intercept_
+
+figure = plt.figure()
+ax = Axes3D(figure, elev=-152, azim=-26, auto_add_to_figure=False)
+figure.add_axes(ax)
+xx = np.linspace(X_new[:, 0].min() - 2, X_new[:, 0].max() + 2, 50)
+yy = np.linspace(X_new[:, 1].min() - 2, X_new[:, 1].max() + 2, 50)
+
+XX, YY = np.meshgrid(xx, yy)
+ZZ = (coef[0] * XX + coef[1] * YY + intercept) / -coef[2]
+ax.plot_surface(XX, YY, ZZ, rstride=8, cstride=8, alpha=0.3)
+ax.scatter(X_new[mask, 0], X_new[mask, 1], X_new[mask, 2], c='b',
+           s=60, edgecolor='k')
+ax.scatter(X_new[~mask, 0], X_new[~mask, 1], X_new[~mask, 2], c='r', marker='^',
+           s=60, edgecolor='k')
+
+ax.set_xlabel("특성0")
+ax.set_ylabel("특성1")
+ax.set_zlabel("특성1 ** 2")
+plt.show()
+
+#86
+
+ZZ = YY ** 2
+dec = linear_svm_3d.decision_function(np.c_[XX.ravel(), YY.ravel(), ZZ.ravel()])
+plt.contourf(XX, YY, dec.reshape(XX.shape), levels=[dec.min(), 0, dec.max()],
+             cmap=mglearn.cm2, alpha=0.5)
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+plt.show()
+
+
