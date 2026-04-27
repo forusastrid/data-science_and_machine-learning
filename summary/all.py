de191142b5302a2b8e34f7df944968373bdbb332
김희성 <forusastrid@gmail.com>
2026-04-27 11:11:11 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- summary/all.py
+++ summary/all.py
@@ -893,3 +893,278 @@ plt.show()
 #98
 
 mglearn.plots.plot_two_hidden_layer_graph()
+
+#99
+
+from sklearn.nerual_network import MLPClassifier
+from sklearn.dadtasets import make_moons
+
+X, y = make_moons(n_samples=100, noise=0.25, random_state=3)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y,
+                                                    random_state=42)
+mlp = MLPClassifier(solver='lbfgs', random_state=0).fit(X_train, y_train)
+mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=.3)
+mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+plt.show()
+
+#100
+
+mlp = MLPClassifier(solver='lbfgs', random_state=0, hidden_layer_sizes=[10],
+                    max_iter=1000)
+mlp.fit(X_train, y_train)
+mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=.3)
+mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+plt.show()
+
+#101
+
+mlp = MLPClassifier(solver='lbfgs', random_state=0,
+                    hidden_layer_sizes=[10, 10], max_iter=1000)
+mlp.fit(X_train, y_train)
+mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=.3)
+mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+plt.show()
+
+#102
+
+mlp = MLPClassifier(solver='lbfgs', activation='tanh',
+                    random_state=0, hidden_layer_sizes=[10, 10], max_iter=1000)
+mlp.fit(X_train, y_train)
+mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=.3)
+mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+plt.show()
+
+#103
+
+fig, axes = plt.subplots(2, 4, figsize=(20,8))
+for axx, n_hidden_nodes in zip (axes, [10, 100]):
+    for ax, alpha in zip(axx, [0.0001, 0.01, 0.1, 1]):
+        mlp = MLPClassifier(solver="lbfgs", random_state=0, 
+                            hidden_layer_sizes=[n_hidden_nodes, n_hidden_nodes],
+                              alpha=alpha, max_iter=1000)
+        mlp.fit(X_train, y_train)
+        mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=0.3, ax=ax)
+        mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train, ax=ax)
+        ax.set_title('n_hidden=[{}, {}]\nalpha={:.4f}'.format(
+            n_hidden_nodes, n_hidden_nodes, alpha))
+        
+#104
+
+fig, axes = plt.subplots(2, 4, figsize=(20,8))
+for i , ax in enumerate(axes.ravel()):
+    mlp = MLPClassifier(solver="lbfgs", random_state=i, 
+                        hidden_layer_sizes=[100, 100])
+    mlp.fit(X_train, y_train)
+    mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=.3, ax=ax)
+    mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train, ax=ax)
+
+#105
+
+print("유방암 데이터의 특성별 최대값:\n", cancer.data.max(axis=0))
+
+#106
+
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, cancer.target, random_state=0)
+
+mlp = MLPClassifier(random_state=42)
+mlp.fit(X_train, y_train)
+
+print("훈련 세트 정확도: {:.2f}".format(mlp.score(X_train, y_train)))
+print("테스트 세트 정확도: {:.2f}".format(mlp.score(X_test, y_test)))
+
+#107
+
+mean_on_train = X_train.mean(axis=0)
+std_on_train = X_train.std(axis=0)
+
+X_train_scaled = (X_train - mean_on_train) / std_on_train
+
+X_test_scaled = (X_test - mean_on_train) / std_on_train
+
+mlp = MLPClassifer(random_state=0)
+mlp.fit(X_train_scaled, y_train)
+
+print("훈련 세트 정확도 : {:3f}". format(mlp.score(X_train_scaled, y_train)))
+print("테스트 세트 정확도 : {:3f}". format(mlp.score(X_test_scaled, y_test)))
+
+#108
+
+mlp = MLPClassifer(max_iter=1000, random_state=0)
+mlp.fit(X_train_scaled, y_train)
+
+print("훈련 세트 정확도 : {:3f}". format(mlp.score(X_train_scaled, y_train)))
+print("테스트 세트 정확도 : {:3f}". format(mlp.score(X_test_scaled, y_test)))
+
+#109
+
+mlp = MLPClassifer(max_iter=1000, alpha=1, random_state=0)
+mlp.fit(X_train_scaled, y_train)
+
+print("훈련 세트 정확도 : {:3f}". format(mlp.score(X_train_scaled, y_train)))
+print("테스트 세트 정확도 : {:3f}". format(mlp.score(X_test_scaled, y_test)))
+
+#110
+
+mlp.coefs_[0].std(axis=1), mlp.coefs_[0].var(axis=0)
+
+#111
+
+plt.figure(figsize=(20, 5))
+plt.imshow(mlp.coefs_[0], interpolation='none', cmap='viridis')
+plt.yticks(range(30), cancer.feature_names)
+plt.xlabel("은닉 유닛")
+plt.ylabel("입력 특성")
+plt.colorbar()
+plt.show()
+
+#112
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.datasets import make_circles
+X, y = make_circles(noise=0.25, factor=0.5, random_state=1)
+
+y_named = np.array(['blue', 'red'])[y]
+
+X_train, X_test, y_train, y_test , y_train_named, y_test_named = \
+    train_test_split(X, y_named, y, random_state=0)
+
+gbrt = GradientBoostingClassifier(random_state=0)
+gbrt.fit(X_train, y_train_named)
+
+#113
+
+print("X_test.shape:" , X_test.shape)
+print("결정 함수 결과 형태", gbrt.decision_function(X_test).shape)
+
+#114
+
+print("결정 함수:\n",  gbrt.decision_function(X_test)[:6])
+
+#115
+
+print("임계치와 결정 함수 결과 비교:\n",
+      gbrt.decision_function(X_test) > 0)
+print("예측 : \n", gbrt.predict(X_test))
+
+#116
+
+greater_zero = (gbrt.decision_function(X_test) > 0).astype(int)
+
+pred = gbrt.classes_[greater_zero]
+print("pred는 예측 결과와 같다:",
+      np.all(pred == gbrt.predict(X_test)))
+
+#117
+
+decision_function = gbrt.decision_function(X_test)
+print("결정함수 최소값 : {:.2f}, 최대값 : {:.2f}".format(
+    np.min(decision_function), np.max(decision_function)))
+
+#118
+
+fig, axes = plt.subplots(1, 2, figsize=(13, 5))
+
+mglearn.tools.plot_2d_separator(gbrt, X, ax=axes[0], alpha=.4,
+                                fill=True, cm=mglearn.cm2)
+scores_image = mglearn.tools.plot_2d_scores(gbrt, X, ax=axes[1],
+                                            alpha=.4, cm=mglearn.ReBl)
+for ax in axes:
+    mglearn.discrete_scatter(X_test[:, 0], X_test[:, 1], y_test,
+                             markers='^', ax=ax)
+    ax.set_xlabel("특성 0")
+    ax.set_ylabel("특성 1")
+cbar = plt.colorbar(scores_image, ax=axes.tolist())
+cbar.set_alpha(1)
+fig.draw_without_rendering()
+axes[0].legend(["테스트 클래스 0", "테스트 클래스 1", "훈련 클래스 0",
+                 "훈련 클래스 1", ], ncol=4, loc=(.1, 1.1))
+plt.show()
+
+#119
+
+print("확률 값의 형태:", gbrt.predict_proba(X_test).shape)
+
+#120
+
+print("Predicted probabilities:\n",
+      gbrt.predict_proba(X_test[:6]))
+
+#121
+
+fig, axes = plt.subplots(1, 2, figsize=(13, 5))
+
+mglearn.tools.plot_2d_separator(
+    gbrt, X, ax=axes[0], alpha=.4, fill=True, cm=mglearn.cm2)
+scores_image = mglearn.tools.plot_2d_scores(
+    gbrt, X, ax=axes[1], alpha=.5, cm=mglearn.ReBl, function='predict_proba')
+
+for ax in axes:
+    mglearn.discrete_scatter(X_test[:, 0], X_test[:, 1], y_test,
+                             markers='^', ax=ax)
+    mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train,
+                             markers='o', ax=ax)
+    ax.set_xlabel("특성 0")
+    ax.set_ylabel("특성 1")
+
+cbar = plt.colorbar(scores_image, ax=axes.tolist())
+cbar.set_alpha(1)
+fig.draw_without_rendering()
+axes[0].legend(["테스트 클래스 0", "테스트 클래스 1", "훈련 클래스 0",
+                "훈련 클래스 1"], ncol=4, loc=(.1, 1.1))
+plt.show()
+
+#122
+
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(
+    iris.data, iris.target, random_state=42)
+
+gbrt = GradientBoostingClassifier(learning_rate=0.01, random_state=0)
+gbrt.fit(X_train, y_train)
+
+#123
+
+print("결정 함수의 결과 형태:", gbrt.decision_function(X_test).shape)
+print("결정 함수 결과:\n", gbrt.decision_function(X_test)[:6, :])
+
+#124
+
+print("가장 큰 결정 함수의 인덱스:\n",
+      np.argmax(gbrt.decision_function(X_test), axis=1))
+print("예측:\n", gbrt.predict(X_test))
+
+#125
+
+print("예측 확률:\n", gbrt.predict_proba(X_test)[:6])
+print("합:", gbrt.predict_proba(X_test)[:6].sum(axis=1))
+
+#126
+
+print("가장 큰 예측 확률의 인덱스:\n",
+      np.argmax(gbrt.predict_proba(X_test), axis=1))
+print("예측:\n", gbrt.predict(X_test))
+
+#127
+
+logreg = LogisticRegression(max_iter=1000)
+
+named_target = iris.target_names[y_train]
+logreg.fit(X_train, named_target)
+print("훈련 데이터에 있는 클래스 종류:", logreg.classes_)
+print("예측:", logreg.predict(X_test)[:10])
+argmax_dec_func = np.argmax(logreg.decision_function(X_test), axis=1)
+print("가장 큰 결정 함수의 인덱스:", argmax_dec_func[:10])
+print("인덱스를 classes_에 연결:",
+      logreg.classes_[argmax_dec_func][:10])
